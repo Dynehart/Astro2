f09a0e1780b3fb622f67c1c149371892cfa4f21d
Jins243 <95927892+Jins243@users.noreply.github.com>
2024-04-07 22:58:49 +0200

    Update command.ts

--- src/modules/command.ts
+++ src/modules/command.ts
@@ -156,7 +156,7 @@ class commandGroup {
         })
         this.subcommandgroups.forEach(subcommandgroup => {
             if (subcommandgroup.name === commandName || subcommandgroup.aliases.some(alias => alias == commandName)) {
-                subcommandgroup.call(args[0].lowercase ?? "", args.slice(1), message, d, `${origin}${this.name}${space}`, false)
+                subcommandgroup.call((args[0] ?? {lowercase: ""}).lowercase, args.slice(1), message, d, `${origin}${this.name}${space}`, false)
                 validCommand = true
             }
         })
