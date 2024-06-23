1e4db51f43b38b735c461abc45d56fe202d120b0
Jins243 <lsp@kmlj.de>
2024-06-23 20:19:03 +0200

    d

--- src/bot.ts
+++ src/bot.ts
@@ -173,7 +173,7 @@ bot.on("interactionCreate", interaction => {
                         })
                             .catch(err => {
                                 console.log(err)
-                                sendMessage(logchannel, `Yo <@397435995429011467> your code is shit check this out (button):\n${interaction.createdTimestamp})}\n${d}\n${interaction.customId}`)
+                                sendMessage(logchannel, `Yo <@397435995429011467> your code is shit check this out (button):\n${interaction.createdTimestamp}\n${d}\n${interaction.customId}`)
                             })
                     }
                     else {
@@ -196,7 +196,7 @@ bot.on("interactionCreate", interaction => {
                         })
                             .catch(err => {
                                 console.log(err)
-                                sendMessage(logchannel, `Yo <@397435995429011467> your code is shit check this out (command):\n${interaction.createdAt.getMilliseconds()}\n${d}\n${interaction.commandName}`)
+                                sendMessage(logchannel, `Yo <@397435995429011467> your code is shit check this out (command):\n${interaction.createdTimestamp}\n${d}\n${interaction.commandName}`)
                             })
                     }
                     else {
